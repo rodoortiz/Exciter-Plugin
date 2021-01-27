a918bf3d881edf5826cc951c7868c73d671abd96
Rodolfo Ortiz <rodolfortiz94@gmail.com>
2021-01-26 22:56:16 -0600

    Add Sliders

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -13,9 +13,17 @@
 ExciterAudioProcessorEditor::ExciterAudioProcessorEditor (ExciterAudioProcessor& p)
     : AudioProcessorEditor (&p), audioProcessor (p)
 {
-    // Make sure that before the constructor has finished, you've set the
-    // editor's size to whatever you need it to be.
-    setSize (400, 300);
+    frequencySlider = std::make_unique<Slider>();
+    frequencySlider->setSliderStyle(Slider::Rotary);
+    frequencySlider->setTextBoxStyle(Slider::TextBoxBelow, false, 60, 15);
+    addAndMakeVisible(frequencySlider.get());
+    
+    exciterSlider = std::make_unique<Slider>();
+    exciterSlider->setSliderStyle(Slider::Rotary);
+    exciterSlider->setTextBoxStyle(Slider::TextBoxBelow, false, 60, 15);
+    addAndMakeVisible(exciterSlider.get());
+    
+    setSize (500, 400);
 }
 
 ExciterAudioProcessorEditor::~ExciterAudioProcessorEditor()
@@ -27,14 +35,14 @@ void ExciterAudioProcessorEditor::paint (juce::Graphics& g)
 {
     // (Our component is opaque, so we must completely fill the background with a solid colour)
     g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
-
-    g.setColour (juce::Colours::white);
-    g.setFont (15.0f);
-    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
 }
 
 void ExciterAudioProcessorEditor::resized()
 {
-    // This is generally where you'll want to lay out the positions of any
-    // subcomponents in your editor..
+    frequencySlider->setSize(100, 100);
+    frequencySlider->setCentreRelative(0.3f, 0.5f);
+    
+    exciterSlider->setSize(100, 100);
+    exciterSlider->setCentreRelative(0.6f, 0.5f);
+    
 }
--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -25,9 +25,9 @@ public:
     void resized() override;
 
 private:
-    // This reference is provided as a quick way for your editor to
-    // access the processor object that created it.
     ExciterAudioProcessor& audioProcessor;
-
+    
+    std::unique_ptr<Slider> frequencySlider, exciterSlider;
+    
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (ExciterAudioProcessorEditor)
 };
